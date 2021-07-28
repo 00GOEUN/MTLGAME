ee3b14274291ab5bb3affd129cd5152a5252b59b
고은 최 <20190957@g.kit.ac.kr>
2021-07-28 12:49:12 +0900

    수정중..

--- MiracleTakeALook/MiracleTakeALook/ObjectManager.cpp
+++ MiracleTakeALook/MiracleTakeALook/ObjectManager.cpp
@@ -30,6 +30,31 @@ ObjectManager::~ObjectManager()
 {
 	Release();
 }
+void ObjectManager::AddObject(Object* _pObject)
+{
+	//** 지금 전달받은 매개변수의 키값을 확인한다.
+	//** 만약 기존에 키값이 존재 하지 않는다면 end() 를 반환 함.
+	map<string, list<Object*>>::iterator iter = ObjectList.find(_pObject->m_strKey());
+
+	//** iter가 ObjectList.end()와 같다면 기존에 키값이 존재하지 않음.
+	//** 만약 존재하지 않는다면....
+	if (iter == ObjectList.end())
+	{
+		//** map에 넣을 리스트를 생성.
+		list<Object*> TempList;
+
+		//** 생성한 리스트에 전달받은 오브젝트를 추가.
+		TempList.push_back(_pObject);
+
+		//** 오브젝트를 추가한 리스트를 오브젝트의 키값으로 map에 추가. 
+		ObjectList.insert(make_pair(_pObject->m_strKey(), TempList));
+	}
+
+	//** 만약 기존에 동일한 키값이 존재 한다면....
+	else
+		//** 동일한 키값에 있는 리스트에 바로 추가해준다.
+		iter->second.push_back(_pObject);
+}
 
 void ObjectManager::Initialize()
 {
@@ -93,12 +118,6 @@ void ObjectManager::Initialize()
 		ObjectManager::GetInstance()->AddObject(
 			ObjectFactory<SkillBullet>::CreateObject());
 	}
-
-	
-
-
-
-
 
 
 
@@ -446,29 +465,4 @@ void ObjectManager::CreateEnemy()
 	
 }
 
-void ObjectManager::AddObject(Object* _pObject)
-{
-	//** 지금 전달받은 매개변수의 키값을 확인한다.
-	//** 만약 기존에 키값이 존재 하지 않는다면 end() 를 반환 함.
-	map<string, list<Object*>>::iterator iter = ObjectList.find(_pObject->m_strKey());
-
-	//** iter가 ObjectList.end()와 같다면 기존에 키값이 존재하지 않음.
-	//** 만약 존재하지 않는다면....
-	if (iter == ObjectList.end())
-	{
-		//** map에 넣을 리스트를 생성.
-		list<Object*> TempList;
-
-		//** 생성한 리스트에 전달받은 오브젝트를 추가.
-		TempList.push_back(_pObject);
-
-		//** 오브젝트를 추가한 리스트를 오브젝트의 키값으로 map에 추가. 
-		ObjectList.insert(make_pair(_pObject->m_strKey(), TempList));
-	}
-
-	//** 만약 기존에 동일한 키값이 존재 한다면....
-	else
-		//** 동일한 키값에 있는 리스트에 바로 추가해준다.
-		iter->second.push_back(_pObject);
-}
 
