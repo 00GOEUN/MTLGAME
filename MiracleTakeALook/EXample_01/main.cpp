3187a5a7cd108bb730e8d31fc16ac616ef7b7c29
고은 최 <20190957@g.kit.ac.kr>
2021-08-05 12:54:31 +0900

    아직

--- MiracleTakeALook/EXample_01/main.cpp
+++ MiracleTakeALook/EXample_01/main.cpp
@@ -43,27 +43,42 @@ int main(void)
 	//** 읽어온 데이터를 각각의 점수에 따라 내림차순 정렬을 할것이다.
 	//** 국어, 영어, 수학 점수별로 선택한 후 선택된 과목의 점수를 내림차순 정렬할것.
 	
+	int pick;
 
 	while (true)
 	{
-		int pick;
-		cout << "1. 국어 2. 영어 3. 수학 4. 종료\n선택: ";
+		cout << "1. 국어 2. 영어 3. 수학 4. 종료\n선택: "; cin >> pick;
 		
 	
 	}
 
 	switch (pick)
 	{
-	case 1: // 국어 
+	case 1: // ??
 		
 		break;
-	case 2: // 영어
-		
+	case 2: // 정렬
+		pick = 0;
+		cout << "1. 국어 점수 정렬" << endl;
+		cout << "2. 영어 점수 정렬" << endl;
+		cout << "3. 수학 점수 정렬" << endl;
+		cout << "선택: "; cin >> pick;
+		switch (pick)
+		{
+		case 1:
+
+
+			break;
+		case 2:
+
+			break;
+		case 3:
+
+			break;
+		}
+
 		break;
-	case 3: // 수학
-		
-		break;
-	case 4: // 종료
+	case 3: // 종료
 		exit(NULL);
 		break;
 	}
